1bf2fd97548616d275fda5dea26dada4b795cbe6
MarioSimou <marios.simouu@gmail.com>
2022-06-05 19:31:29 +0100

    feat: wip 3

--- apps/blogs/next.config.js
+++ apps/blogs/next.config.js
@@ -1,5 +1,8 @@
 const withTM = require('next-transpile-modules')(['ui'])
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = withTM({
   reactStrictMode: true,
+  assetPrefix: isProd ? 'http://localhost:3001': ''
 })
